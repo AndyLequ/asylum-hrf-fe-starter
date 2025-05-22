3da965425c72ea70319c82a8985cd4a7b6e18a35
Andy Le <andylequ@gmail.com>
2025-05-22 13:30:43 -0500

    got profile information like picture, username, user email to show in profile page

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -12,20 +12,29 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const Profile = () => {
   // TODO: Replace these with functionality from Auth0
-  const isLoading = false;
-  const user = true;
-  const username = ""
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+  } = useAuth0();
 
   if (isLoading || !user) {
     return <div className='text-center p-4'>Loading...</div>;
   }
 
   return (
-    <div>
-      <img src="" alt="" />
-      Profile Page
-      {username}
-      <button>Logout</button>
+    <div className='max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 p-4'>
+      <div className='flex justify-center my-6'>
+        <div className='w-24 h-24 rounded-full overflow-hidden border-white shadow-lg'>
+          <img 
+          className="w-full h-full object-cover"
+          src={user.picture} 
+          alt="profile picture" />
+        </div>
+      </div>
+      <h2 className='text-3xl font-bold'>{user.name}</h2>
+      <p className='p-4 text-gray-600/85'>{user.email}</p>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout</button>
       </div>
   );
 };
